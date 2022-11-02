153be79dc0c1a2caa73c5e0b9dc78a9d58fb9805
Imhandegbelo <okogbe@outloo.com>
2022-11-02 04:18:24 +0100

    added projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,62 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
+(function ($) {
 
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
+  // Show current year
+  $("#current-year").text(new Date().getFullYear());
 
-(function($) {
+  // Animate to section when nav is clicked
+  $('header a').click(function (e) {
 
-    // Show current year
-    $("#current-year").text(new Date().getFullYear());
+    // Treat as normal link if no-scroll class
+    if ($(this).hasClass('no-scroll')) return;
 
-    // Remove no-js class
-    $('html').removeClass('no-js');
+    e.preventDefault();
+    var heading = $(this).attr('href');
+    var scrollDistance = $(heading).offset().top;
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    $('html, body').animate({
+      scrollTop: scrollDistance + 'px'
+    }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+    // Hide the menu once clicked if mobile
+    if ($('header').hasClass('active')) {
+      $('header, body').removeClass('active');
+    }
+  });
 
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+  // Create timeline
+  $('#experience-timeline').each(function () {
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+    $this = $(this); // Store reference to this
+    $userContent = $this.children('div'); // user content
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
+    // Create each timeline block
+    $userContent.each(function () {
+      $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
     });
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
-
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
-
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
-
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
-
+    // Add icons to each block
+    $this.find('.vtimeline-point').each(function () {
+      $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
     });
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
+    // Add dates to the timeline if exists
+    $this.find('.vtimeline-content').each(function () {
+      var date = $(this).data('date');
+      if (date) { // Prepend if exists
+        $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
+      }
     });
 
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
+  });
 
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
+  // Open mobile menu
+  $('#mobile-menu-open').click(function () {
+    $('header, body').addClass('active');
+  });
+
+  // Close mobile menu
+  $('#mobile-menu-close').click(function () {
+    $('header, body').removeClass('active');
+  });
 
 })(jQuery);
